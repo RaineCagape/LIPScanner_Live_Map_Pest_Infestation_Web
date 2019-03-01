6fffae3b9d0204d8005a7097f13b725af80c54f0
RaineCagape <31531825+RaineCagape@users.noreply.github.com>
2019-03-02 01:36:13 +0800

    Update map.php
    Request user location syntax

--- map/map.php
+++ map/map.php
@@ -19,15 +19,36 @@
   <body>
     <div id="map"></div>
     <script>
-      var map,userLocate;
-      //syntax will be change for getting the location of the user (see documentation)
-      var userLocate = {lat:7.055630, lng:125.610491}; 
+      var map;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
-          center: userLocate,
+          center: {lat: -34.397, lng: 150.644},
           zoom: 15
         });
-        var marker = new google.maps.Marker({position: userLocate, map: map});
+        infoWindow = new google.maps.InfoWindow;
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var userLatLng = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+            var marker = new google.maps.Marker({position: userLatLng, map: map});
+            infoWindow.open(map);
+            map.setCenter(userLatLng);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+      function handleLocationError(browserHasGeolocation, infoWindow, userLatLng) {
+        infoWindow.setPosition(userLatLng);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
       }
     </script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC6OiUJeuWP82cOlwxHzAFlLlx51XS6mn4&callback=initMap"
