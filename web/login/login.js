44a329072cb86aca5c39f62862da4f1a1caa6a8a
RaineCagape <31531825+RaineCagape@users.noreply.github.com>
2019-04-16 03:27:22 +0800

    Ongoing Login Fix

--- web/login/login.js
+++ web/login/login.js
@@ -1,23 +1,5 @@
 (function() {
 
-  firebase.auth().onAuthStateChanged(firebaseUser => {
-    firebaseUser = firebase.auth().currentUser;
-    if(firebaseUser){
-      console.log(firebaseUser);
-      document.getElementById('log-out').style.display = 'block';
-      document.getElementById('logging-in').style.display = 'none';
-      document.getElementById('log-in').style.display = 'none';
-      document.getElementById('logged-in').style.display ='block';
-    }
-    else{
-      console.log('not logged in');
-      document.getElementById('logging-in').style.display = 'block';
-      document.getElementById('logged-in').style.display = 'none';
-      document.getElementById('log-out').style.display = 'none';
-      document.getElementById('log-in').style.display = 'block';
-    }
-  });
-
   const inputEmail = document.getElementById('email');
   const inputPassword = document.getElementById('password');
   const btnLogin = document.getElementById('btn-login');
@@ -38,6 +20,25 @@
   btnLogout.addEventListener('click', e => {
     firebase.auth().signOut();
     console.log('not logged in');
+  });
+
+  
+  firebase.auth().onAuthStateChanged(firebaseUser => {
+    firebaseUser = firebase.auth().currentUser;
+    if(firebaseUser){
+      console.log(firebaseUser);
+      document.getElementById('log-out').style.display = 'block';
+      document.getElementById('logging-in').style.display = 'none';
+      document.getElementById('log-in').style.display = 'none';
+      document.getElementById('logged-in').style.display ='block';
+    }
+    else{
+      console.log('not logged in');
+      document.getElementById('logging-in').style.display = 'block';
+      document.getElementById('logged-in').style.display = 'none';
+      document.getElementById('log-out').style.display = 'none';
+      document.getElementById('log-in').style.display = 'block';
+    }
   });
 
 }());
