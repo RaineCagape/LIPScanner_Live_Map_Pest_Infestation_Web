aba7dc13d9a223e8849a45e3a0419558fc82ff06
RaineCagape <31531825+RaineCagape@users.noreply.github.com>
2019-04-15 22:05:08 +0800

    Ongoing login fix

--- web/login/login.js
+++ web/login/login.js
@@ -1,27 +1,5 @@
 (function() {
 
-  const inputEmail = document.getElementById('email');
-  const inputPassword = document.getElementById('password');
-  const btnLogin = document.getElementById('btn-login');
-  const btnLogout =document.getElementById('btn-logout');
-
-  btnLogin.addEventListener('click', e => {
-    const email = inputEmail.value;
-    const pass = inputPassword.value;
-    const auth = firebase.auth();
-    
-    const promise = auth.signInWithEmailAndPassword(email,pass);
-    auth.setPersistence(firebase.auth.Auth.Persistence.SESSION);
-    console.log('logged in');
-    promise.catch(e => console.log(e.message));
-    // session.catch(e => console.log(e.message));
-  });
-
-  btnLogout.addEventListener('click', e => {
-    firebase.auth().signOut();
-    console.log('not logged in');
-  });
-
   firebase.auth().onAuthStateChanged(firebaseUser => {
     firebaseUser = firebase.auth().currentUser;
     if(firebaseUser){
@@ -38,6 +16,28 @@
       document.getElementById('log-out').style.display = 'none';
       document.getElementById('log-in').style.display = 'block';
     }
+  });
+
+  const inputEmail = document.getElementById('email');
+  const inputPassword = document.getElementById('password');
+  const btnLogin = document.getElementById('btn-login');
+  const btnLogout =document.getElementById('btn-logout');
+
+  btnLogin.addEventListener('click', e => {
+    const email = inputEmail.value;
+    const pass = inputPassword.value;
+    const auth = firebase.auth();
+    
+    const promise = auth.signInWithEmailAndPassword(email,pass);
+    // auth.setPersistence(firebase.auth.Auth.Persistence.SESSION);
+    console.log('logged in');
+    promise.catch(e => console.log(e.message));
+    // session.catch(e => console.log(e.message));
+  });
+
+  btnLogout.addEventListener('click', e => {
+    firebase.auth().signOut();
+    console.log('not logged in');
   });
 
 }());
