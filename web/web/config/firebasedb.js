7fed62f2902ea74d30eaf0785f2984064d85609d
RaineCagape <31531825+RaineCagape@users.noreply.github.com>
2019-10-17 17:30:31 +0800

    Change Database

---
+++ web/web/config/firebasedb.js
@@ -0,0 +1,14 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDiv-VUSzlsX2F1EmgNSkzu18dgyk1ZcFs",
+    authDomain: "capstone-project-e7f9d.firebaseapp.com",
+    databaseURL: "https://capstone-project-e7f9d.firebaseio.com",
+    projectId: "capstone-project-e7f9d",
+    storageBucket: "capstone-project-e7f9d.appspot.com",
+    messagingSenderId: "209134216811",
+    appId: "1:209134216811:web:b3235327f1a15f5b69732e",
+    measurementId: "G-2CW5DXBX84"
+
+  };
+  firebase.initializeApp(config);
+  
